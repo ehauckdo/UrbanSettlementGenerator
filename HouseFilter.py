e364f2f52952a685c4a006404d814557d16c07fa
ehauckdo <eduardohauck@gmail.com>
2019-01-25 16:01:41 +0900

    generate actual buildings intead of solid blocks

--- HouseFilter.py
+++ HouseFilter.py
@@ -26,8 +26,21 @@ def generateHouse(level,box,options):
 
 
 def generateMatrix(width, depth, height, options):
-	matrix = [[[options["Walls"].ID for z in range(depth)] for x in range(width)] for y in range(height)]
+	matrix = [[[0 for z in range(depth)] for x in range(width)] for y in range(height)]
 
+	# walls along x axis
+	for x in range(width):
+		for y in range(height):
+			matrix[y][x][0] = options["Walls"].ID
+			matrix[y][x][depth-1] = options["Walls"].ID
+
+	# walls along z axis
+	for z in range(depth):
+		for y in range(height):
+			matrix[y][0][z] = options["Walls"].ID
+			matrix[y][width-1][z] = options["Walls"].ID
+		
+	# ceiling on every block on uppermost y axis value
 	for x in range(len(matrix[height-1])):
 		for z in range(len(matrix[height-1][x])):
 				matrix[height-1][x][z] = options["Ceiling"].ID
