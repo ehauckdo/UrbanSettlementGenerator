22020ef597428bed3f31c0c0e8d971884312d9a6
ehauckdo <eduardohauck@gmail.com>
2019-04-03 15:10:15 +0900

    Cleaning generate building file

--- Generators/GenerateBuilding.py
+++ Generators/GenerateBuilding.py
@@ -6,37 +6,6 @@ import math
 import RNG
 import logging
 
-def generateHospital(matrix, h_min, h_max, x_min, x_max, z_min, z_max):
-
-	utilityFunctions.cleanProperty(matrix, h_min+1, h_max, x_min, x_max, z_min, z_max)
-
-	hospitalPartition = utilityFunctions.getSubsection(x_min, x_max, z_min, z_max, 0.8)
-	x_min = hospitalPartition[0]
-	x_max = hospitalPartition[1]
-	z_min = hospitalPartition[2]
-	z_max = hospitalPartition[3]
-
-	if h_max-h_min < 20 or x_max-x_min < 8 or z_max-z_min < 8:
-		return matrix
-
-	#wall = (43,random.randint(0,8))
-	wall = (43,15)
-	ceiling = wall
-	floor = wall
-	door = (0,0)
-
-	floor_size = 8
-	if h_max-h_min > 82:
-		h_max = h_min+82
-
-	while (h_max-h_min) % floor_size != 0:
-		h_max -= 1
-
-	generateBuildingWalls(matrix, h_min, h_max, floor_size, x_min, x_max, z_min, z_max, wall)
-	generateBuildingWindows(matrix, h_min, h_max, floor_size, x_min, x_max, z_min, z_max)
-	generateDoor(matrix, x_min, x_max, z_min, z_max, h_min, h_max, (0,0), (0,0))
-	generateStairs(matrix, h_min, h_max, floor_size, x_min, x_max, z_min, z_max, wall)
-
 def generateBuilding(matrix, h_min, h_max, x_min, x_max, z_min, z_max):
 
 	building = utilityFunctions.dotdict()
@@ -66,26 +35,24 @@ def generateBuilding(matrix, h_min, h_max, x_min, x_max, z_min, z_max):
 		h_max -= 1
 
 	generateBuildingWalls(matrix, h_min, h_max, floor_size, x_min, x_max, z_min, z_max, wall)
-	#if RNG.random() > 0.5:
-	building.orientation = "x"
-	building.door = generateDoor_x(matrix, h_min, h_max, x_min, x_max, z_min, z_max)
-	if building.door[2] == z_min:
-		building.entranceLot = (building.door[0], building.door[1], building.area.z_min)
-	else:
-		building.entranceLot = (building.door[0], building.door[1], building.area.z_max)
-	generateBuildingWindows_z(matrix, h_min, h_max, floor_size, x_min, x_max, z_min, z_max)
-	#else:
-	#	building.orientation = "z"
-	#	building.door = generateDoor_x(matrix, h_min, h_max, x_min, x_max, z_min, z_max)
-	#	if building.door[1] == x_min:
-	#		building.entranceLot = (building.door[0], building.area.x_min, building.door[2])
-	#	else:
-	#		building.entranceLot = (building.door[0], building.area.x_max, building.door[2])
-	#	generateBuildingWindows_z(matrix, h_min, h_max, floor_size, x_min, x_max, z_min, z_max)
+	generateFloorsDivision(matrix, h_min, h_max, floor_size, x_min, x_max, z_min, z_max, wall)
 
-	generateStairs(matrix, h_min, h_max, floor_size, x_min, x_max, z_min, z_max, wall)
-	generateFloorPlan(matrix, h_min, h_max, floor_size, x_min, x_max, z_min, z_max, wall)
-	generateInterior(matrix, h_min, h_max, floor_size, x_min, x_max, z_min, z_max-6)
+	building.orientation = getOrientation()
+
+	if building.orientation == "S":
+		door_x = RNG.randint(x_min+1, x_max-1)
+		door_z = z_max
+		generateDoor(matrix, h_min+1, door_x, door_z, (64,9), (64,3))
+		building.entranceLot = (h_min+1, door_x, building.area.z_max)
+
+		# apartment windows
+		generateBuildingWindows_AlongZ(matrix, h_min, h_max, floor_size, x_min, x_max, z_min)
+		# corridor windows
+		generateBuildingWindows_AlongZ(matrix, h_min, h_max, floor_size, x_min, x_max, z_max)
+
+		generateStairs(matrix, h_min, h_max, floor_size, x_min, x_max, z_min, z_max, wall)
+		generateFloorPlan(matrix, h_min, h_max, floor_size, x_min, x_max, z_min, z_max, wall)
+		generateInterior(matrix, h_min, h_max, floor_size, x_min, x_max, z_min, z_max-6)
 
 	#for h in range(h_min, 100):
 	#	matrix[h][building.entranceLot[1]][building.entranceLot[2]] = (35,2)
@@ -147,19 +114,19 @@ def generateStairs(matrix, h_min, h_max, floor_size, x_min, x_max, z_min, z_max,
 		cur_floor += floor_size
 
 def generateFloorPlan(matrix, h_min, h_max, floor_size, x_min, x_max, z_min, z_max, wall):
-
 	cur_floor = h_min
-	cur_floor_ceiling = cur_floor+floor_size
+	z_wall = z_max-6
+
 	while cur_floor < h_max:
+
 		# generating separating wall between hall and apartments
 		for h in range(cur_floor, cur_floor+floor_size):
 			for x in range(x_min, x_max):
-				matrix.setValue(h, x, z_max-6, wall)
+				matrix.setValue(h, x, z_wall, wall)
 
 		# generating door to the apartment
-		door_x = x_max - ((x_max-x_min)/2)
-		matrix.setValue(cur_floor+2, door_x, z_max-6, (64,8))
-		matrix.setValue(cur_floor+1, door_x, z_max-6, (64,8))
+		x_door = x_max - ((x_max-x_min)/2)
+		generateDoor(matrix, cur_floor+1, x_door, z_wall, (64,9), (64,3))
 
 		cur_floor += floor_size
 
@@ -210,6 +177,7 @@ def generateBuildingWalls(matrix, h_min, h_max, floor_size, x_min, x_max, z_min,
 			matrix.setValue(y,x_max,z, wall)
 			matrix.setValue(y,x_min,z, wall)
 
+def generateFloorsDivision(matrix, h_min, h_max, floor_size, x_min, x_max, z_min, z_max, wall):
 	# floors division
 	cur_floor = h_min
 	while cur_floor <= h_max:
@@ -219,132 +187,37 @@ def generateBuildingWalls(matrix, h_min, h_max, floor_size, x_min, x_max, z_min,
 
 		cur_floor += floor_size
 
+def getOrientation():
+	random = RNG.random()
+	if random > 0:
+		return "S"
+	elif random < 0.5:
+		return "N"
+	elif random < 0.75:
+		return "E"
+	else:
+		return "W"
 
-def generateBuildingWindows(matrix, h_min, h_max, floor_size, x_min, x_max, z_min, z_max):
-	z_window_size = z_max-random.randint(z_min+2, z_max-2)
+def generateDoor(matrix, y, x, z, door_up, door_down):
+	matrix.setValue(y+1, x, z, door_up)
+	matrix.setValue(y, x, z, door_down)
+
+def generateBuildingWindows_AlongZ(matrix, h_min, h_max, floor_size, x_min, x_max, z):
 	x_window_size = x_max-random.randint(x_min+2, x_max-2)
 
 	# windows
 	cur_floor = h_min+1
 	while cur_floor < h_max:
 
-		window_h = cur_floor + int(math.ceil(floor_size/2))
-	 	#width = int(x_max - x_min)
-	 	#middle = x_min + int(width/2)
-		#for x in range(middle - int(x_window_size/2), middle + int(math.ceil(x_window_size/2)),4):
-		for x in range(x_min+1, x_max-1, 3):
-
-			matrix.setValue(window_h, x, z_min, (20,0))
-			matrix.setValue(window_h, x, z_max, (20,0))
-			matrix.setValue(window_h-1, x, z_min, (20,0))
-			matrix.setValue(window_h-1, x, z_max, (20,0))
-
-			matrix.setValue(window_h, x+1, z_min, (20,0))
-			matrix.setValue(window_h, x+1, z_max, (20,0))
-			matrix.setValue(window_h-1, x+1, z_min, (20,0))
-			matrix.setValue(window_h-1, x+1, z_max, (20,0))
-
-		#depth = z_max - z_min
-		#middle = z_min + int(depth/2)
-		#for z in range(middle - int(z_window_size/2), middle + int(math.ceil(z_window_size/2)),4):
-		for z in range(z_min+1, z_max-1, 3):
-
-		 	matrix.setValue(window_h, x_min, z, (20,0))
-		 	matrix.setValue(window_h, x_max, z, (20,0))
-		 	matrix.setValue(window_h-1, x_min, z, (20,0))
-		 	matrix.setValue(window_h-1, x_max, z, (20,0))
-
-		 	matrix.setValue(window_h, x_min, z+1, (20,0))
-		 	matrix.setValue(window_h, x_max, z+1, (20,0))
-		 	matrix.setValue(window_h-1, x_min, z+1, (20,0))
-		 	matrix.setValue(window_h-1, x_max, z+1, (20,0))
-
-		cur_floor += floor_size
-
-def generateBuildingWindows_x(matrix, h_min, h_max, floor_size, x_min, x_max, z_min, z_max):
-	z_window_size = z_max-random.randint(z_min+2, z_max-2)
-
-	# windows
-	cur_floor = h_min+1
-	while cur_floor < h_max:
-
-		window_h = cur_floor + int(math.ceil(floor_size/2))
-	 
-		#depth = z_max - z_min
-		#middle = z_min + int(depth/2)
-		#for z in range(middle - int(z_window_size/2), middle + int(math.ceil(z_window_size/2)),4):
-		for z in range(z_min+1, z_max-1, 3):
-
-		 	matrix.setValue(window_h, x_min, z, (20,0))
-		 	matrix.setValue(window_h-1, x_min, z, (20,0))
-		 	matrix.setValue(window_h, x_min, z+1, (20,0))
-		 	matrix.setValue(window_h-1, x_min, z+1, (20,0))
-
-		cur_floor += floor_size
-
-def generateBuildingWindows_z(matrix, h_min, h_max, floor_size, x_min, x_max, z_min, z_max):
-	x_window_size = x_max-random.randint(x_min+2, x_max-2)
-
-	# windows
-	cur_floor = h_min+1
-	while cur_floor < h_max:
-
-		
 		window_h = cur_floor + int(math.ceil(floor_size/2))
 		for x in range(x_min+2, x_max-1, 3):
 			# apartment windows
-			matrix.setValue(window_h, x, z_min, (20,0))
-			matrix.setValue(window_h-1, x, z_min, (20,0))
-			matrix.setValue(window_h, x+1, z_min, (20,0))
-			matrix.setValue(window_h-1, x+1, z_min, (20,0))
-
-			# corridor windows
-			matrix.setValue(window_h, x, z_max, (20,0))
-			matrix.setValue(window_h-1, x, z_max, (20,0))
-			matrix.setValue(window_h, x+1, z_max, (20,0))
-			matrix.setValue(window_h-1, x+1, z_max, (20,0))
-
+			matrix.setValue(window_h, x, z, (20,0))
+			matrix.setValue(window_h-1, x, z, (20,0))
+			matrix.setValue(window_h, x+1, z, (20,0))
+			matrix.setValue(window_h-1, x+1, z, (20,0))
 
 		cur_floor += floor_size
-
-def generateDoor(matrix, x_min, x_max, z_min, z_max, h_min, h_max, door_up, door_down):
-
-	chance = random.random()
-	if chance < 0.25:
-		pos = random.randint(z_min+1,z_max-1)
-		matrix[h_min+2][x_min][pos] = (64,9)
-		matrix[h_min+1][x_min][pos] = (64,0)
-		return (h_min+1, x_min, pos)
-	elif chance < 0.50:
-		pos = random.randint(x_min+1,x_max-1)
-		matrix[h_min+2][pos][z_min] = (64,8)
-		matrix[h_min+1][pos][z_min] = (64,1)
-		return (h_min+1, pos, z_min)
-	elif chance < 0.75:
-		pos = random.randint(z_min+1,z_max-1)
-		matrix[h_min+2][x_max][pos] = (64,9)
-		matrix[h_min+1][x_max][pos] = (64,2)
-		return (h_min+1, x_max, pos)
-	else:
-		pos = random.randint(x_min+1,x_max-1)
-		matrix[h_min+2][pos][z_max] = (64,8)
-		matrix[h_min+1][pos][z_max] = (64,3)
-		return (h_min+1, pos, z_max)
-
-def generateDoor_x(matrix, h_min, h_max, x_min, x_max, z_min, z_max):
-
-	lader_space = x_max-8
-	pos = RNG.randint(x_min+1, lader_space)
-	#pos = x_min+1
-	#chance = RNG.random()
-	#if chance < 0.50:
-	#matrix[h_min+2][pos][z_min] = (64,8)
-	#matrix[h_min+1][pos][z_min] = (64,1)
-	#return (h_min, pos, z_min)
-	#else:
-	matrix.setValue(h_min+2, pos, z_max, (64,8))
-	matrix.setValue(h_min+1, pos, z_max, (64,8))
-	return (h_min, pos, z_max)
 
 def generateDoor_z(matrix, h_min, h_max, x_min, x_max, z_min, z_max):
 
